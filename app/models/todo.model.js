fd9438bec550d6950ac3531b9182bb55d5c31009
Ali Naeem <alitheflyingtiger111@gmail.com>
2018-09-17 12:15:55 -0700

    controller added and server.js changed

--- app/models/todo.model.js
+++ app/models/todo.model.js
@@ -1,20 +1,21 @@
 var mongoose = require("mongoose");
 var Schema = mongoose.Schema;
-// Defining schema for our Todo API
-var TodoSchema = Schema({
-  todo: {
-    type: String
-  },
-  completed: {
-    type: Boolean,
-    default: false
-  },
-  created_by: {
-    type: Date,
-    default: Date.now
-  }
+
+// TODO schema
+var TodoSchema = new Schema({
+  todo: String,
+  completed: { type: Boolean, default: false },
+  created_by: { type: Date, default: Date.now }
 });
-//Exporting our model
+
+// True since it is a parallel middleware
+TodoSchema.pre("save", function(next, done) {
+  if (!this.todo) {
+    next(new Error("Todo should not be null"));
+  }
+  next();
+});
+
 var TodoModel = mongoose.model("Todo", TodoSchema);
 
 module.exports = TodoModel;
